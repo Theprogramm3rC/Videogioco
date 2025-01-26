2b978adec7b88f0dd2abbb67c11eee4b4aa9caae
Theprogramm3rC <theprogramm3rC@gmail.com>
2025-01-26 13:04:23 +0100

    Android

--- Assets/Android.cs
+++ Assets/Android.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class Android : MonoBehaviour
 {
+    public bool facingleft = true;
     public float moveSpeed = 2f;
     public Transform checkPoint;
     public float distance = 1f;
     public LayerMask layerMask;
     
+
     
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -20,6 +22,28 @@ public class Android : MonoBehaviour
     {
         transform.Translate(Vector2.left* Time.deltaTime * moveSpeed);
 
-        //Physics2D.Raycast()
+        RaycastHit2D hit = Physics2D.Raycast(checkPoint.position, Vector2.down, distance, layerMask);
+
+        if(hit == false && facingleft){
+            transform.eulerAngles = new Vector3(0, -180, 0);
+            facingleft = false;
+
+            
+        }
+        else if(hit == false && facingleft == false){
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            facingleft = true;
+
+        }
     }
+
+    private void OnDrawGizmosSelected(){
+        if(checkPoint == null){
+            return;
+        }
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(checkPoint.position, Vector2.down * distance);
+    }
+    
 }
+
