ff062078a39049150b7750a447b01f19ace1a536
Theprogramm3rC <theprogramm3rc@gmail.com>
2025-01-28 18:53:27 +0100

    yes

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -9,10 +9,29 @@ public class Menu : MonoBehaviour
     }  
     
     public void Settings(){
-
+              
     }
 
     public void Exit(){
-        
+              SceneManager.LoadScene(1);        
     }
+
+    public void DescriptionF(){
+           SceneManager.LoadScene(2);
+    }  
+
+    public void DescriptionA(){
+           SceneManager.LoadScene(3);
+    }
+    public void DescriptionL(){
+           SceneManager.LoadScene(4);
+    }
+
+       public void DescriptionG(){
+           SceneManager.LoadScene(5);
+    }
+
+    public void Inizia(){
+              SceneManager.LoadScene(6);          
+    }  
 }
