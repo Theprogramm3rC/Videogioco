583c0692d367230d8a3137b8342ff02c84c77658
Theprogramm3rC <theprogramm3rc@gmail.com>
2025-03-10 13:40:07 +0100

    ok

--- Assets/SceneManagement2.cs
+++ Assets/SceneManagement2.cs
@@ -1,12 +1,19 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class SceneManagement2 : MonoBehaviour
 {
- public void LoadLevel(){
-    SceneManager.LoadScene(13);
- }
-    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            LoadLevel();
+        }
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(13);
+    }
 }
 
