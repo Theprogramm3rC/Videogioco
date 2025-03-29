d31d2a1f127cc262a5a37edd01d6fa134ad8b440
Theprogramm3rC <theprogramm3rC@gmail.com>
2025-03-29 20:25:33 +0100

    .

---
+++ Assets/rerturnmenu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class rerturnmenu : MonoBehaviour
+{
+
+
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    
+         SceneManager.LoadScene(0);
+    
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
